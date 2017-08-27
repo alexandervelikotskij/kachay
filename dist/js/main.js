01f82dfd12f1dc895d16d61f0787ab0e6813a057
alexandervelikotskij <alexandervelikotskij@gmail.com>
2017-08-27 18:38:23 +0300

    first commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,14 @@
-// Without JQuery
-var slider = new Slider("#ex6");
+// bootstrap range slider  Without JQuery
+var slider = new Slider("#ex6", {
+	tooltip: 'always'
+});
 slider.on("slide", function(sliderValue) {
 	document.getElementById("ex6SliderVal").textContent = sliderValue;
+});
+
+
+// bootstrap range slider2  Without JQuery
+var slider2 = new Slider("#ex7");
+slider2.on("slide", function(sliderValue) {
+	document.getElementById("ex7SliderVal").textContent = sliderValue;
 });
